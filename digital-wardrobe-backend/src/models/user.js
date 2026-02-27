5aea848d2f37b55026f2475e6dd4738762bdd0be
ramyysameh <117130865+ramyysameh@users.noreply.github.com>
2026-02-27 12:27:01 +0200

    Add files via upload

---
+++ digital-wardrobe-backend/src/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {   type: String, 
+  required: true, unique: true, index: true},
+  email: { type: String, required: true, unique: true, index: true },
+  password: { type: String, required: true },
+
+  preferences: {
+    style: [String],
+    favoriteColors: [String]
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
